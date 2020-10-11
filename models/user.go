cc43b3846f1573c1574a7df2868e01b67f25d8c7
bell001 <930987934@qq.com>
2020-10-11 23:15:31 +0800

    paqu

---
+++ models/user.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"DataCertProject/db_mysql"
+	"crypto/md5"
+	"encoding/hex"
+)
+
+type User struct {
+	Id       int    `form:"id"`
+	Phone    string `form:"phone"`
+	Password string `form:"password"`
+}
+
+//保存用户信息的方法：保存用户信息到数据库中
+
+func (u User) SaverUser() (int64, error) {
+	//	1.密码脱敏处理
+	hashMd5 := md5.New()
+	hashMd5.Write([]byte(u.Password))
+	byte := hashMd5.Sum(nil)
+	u.Password = hex.EncodeToString(byte)
+
+	//	2。执行数据库操作
+	row, err := db_mysql.Db.Exec("insert into user (phone,password)"+"values(?,?)", u.Phone, u.Password)
+	if err != nil {
+		return -1, err
+	}
+	id, err := row.RowsAffected()
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func (u User) QueryUser() (*User, error) {
+	row := db_mysql.Db.QueryRow("select phone from user where phone = ? and  password=?",
+		u.Phone, u.Password)
+
+	err := row.Scan(&u.Phone)
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
---
+++ routers/router.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"DataCertProject/controllers"
+	"github.com/astaxie/beego"
+)
+
+//router.go文件的作用：路由功能。用于接收并分发接收到的浏览器的请求，用于匹配请求
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+
+//    用户注册的请求接口
+beego.Router("/user_register", &controllers.RegisterController{})
+
+//    直接登录的页面请求接口
+beego.Router("/login.html", &controllers.LoginController{})
+
+//    用户登录请求接口
+beego.Router("/user_login", &controllers.LoginController{})
+}
